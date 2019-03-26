2a19f3e8d964ee48d053f124618db0794c38d51a
VJ <vishnuvijay@factor5.com.au>
2019-03-26 14:49:43 +1100

    finished 3 command functionalities
    explorer functionality is  the pending one

--- mars_explorer/src/Explorer.java
+++ mars_explorer/src/Explorer.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Explorer {
+	public static String place, report, block, explore;
+	public static ArrayList<String> blockList = new ArrayList<String>();;
 
 	public static void main(String[] args) {
 		String result, input, command;
@@ -22,29 +26,30 @@ public class Explorer {
 		} while (input.equalsIgnoreCase("Y"));
 	}
 
-	static String validateAndPerformCommand(String command) {
+	public static String validateAndPerformCommand(String command) {
+		int x = 0, y = 0;
 		String[] commandArray = command.split("\\s+");
 		if (commandArray.length <= 2) {
 			String commandProvided = commandArray[0];
 			if (commandProvided != "REPORT" && commandArray.length == 2) {
 				String[] commandUnit = commandArray[1].split("\\s*,\\s*");
 				if (commandUnit.length == 2) {
-					String x = commandUnit[0];
-					String y = commandUnit[1];
+					x = parseStrToInt(commandUnit[0].trim());
+					y = parseStrToInt(commandUnit[1].trim());
 				}
 			}
 			switch (commandProvided) {
 
 			case "PLACE":
-				System.out.println("1");
+				currentPlace(x, y);
 				break;
 
 			case "BLOCK":
-				System.out.println("2");
+				updateCurrentBlocks(x, y);
 				break;
 
 			case "REPORT":
-				System.out.println("3");
+				report();
 				break;
 
 			case "EXPLORE":
@@ -56,11 +61,69 @@ public class Explorer {
 				break;
 
 			}
-
 		}
 
 		System.out.println("6");
 		return "";
 
+	}
+
+	public static int parseStrToInt(String str) {
+		if (str.matches("\\d+")) {
+			return Integer.parseInt(str);
+		} else {
+			return -1;
+		}
+	}
+
+	public static boolean validBoundary(int x, int y) {
+		if (x <= 5 && x >= 0 && y <= 5 && y >= 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static void currentPlace(int x, int y) {
+		if (validBoundary(x, y)) {
+			blockList.clear(); //clears all the existing blocks
+			place = "E: (" + x + "," + y + ")";
+			System.out.println(place);
+		} else {
+			System.out.println("The boundary is not valid");
+		}
+	}
+
+	public static void updateCurrentBlocks(int x, int y) {
+		String blockUnits = "(" + x + "," + y + ")";
+		if (validBoundary(x, y)) {
+			if (blockList.size() > -1) {
+				
+				if (blockList.contains(blockUnits)) {
+					System.out.println("This block already exist");
+				} else {
+					blockList.add(blockUnits);
+					System.out.println(blockList.toString());
+				}
+			}else {
+				blockList.add(blockUnits);
+				System.out.println(blockList.toString());
+			}
+
+		} else {
+			System.out.println("The boundary is not valid");
+		}
+	}
+	
+	public static void report() {
+		System.out.println("E: "+ place + "\nB: "+ blockList.toString());
+	}
+	
+	public static void explore(int x, int y) {
+		if(validBoundary(x,y)) {
+			
+		}else {
+			System.out.println("invalid boundary value");
+		}
 	}
 }
